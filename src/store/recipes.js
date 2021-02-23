a12488830d3a43afa9560afc67f993c07c8f726a
Caroline Fassot <fassot.caroline@gmail.com>
2021-02-23 12:12:14 +0100

    create component Recipes Presentation

--- src/store/recipes.js
+++ src/store/recipes.js
@@ -9,8 +9,68 @@ export const recipes = {
 				other:['6 feuilles de menthe', '1/2 citron vert', '10 glaçons']
 			},
 			link:'/about'
+		},
+		mojito: {
+			name: 'Mojito',
+			desc:'Aenean eget mattis lectus. Proin id sollicitudin nisi. Nullam vitae diam vestibulum, pulvinar nulla nec, pellentesque magna. Aenean varius ut lacus laoreet sollicitudin. Morbi et tortor rutrum, rutrum metus sit amet, ornare ligula. Quisque semper sapien a est vestibulum, eu porttitor velit rhoncus. Phasellus nibh orci, facilisis vel elit eu, feugiat pulvinar ipsum. Praesent at magna augue.',
+			ingredient:{
+				alcohol:['4cl de rhum blanc'],
+				juice : ['1 eau gazeuse', '2cl de sirop de sucre de canne'],
+				other:['6 feuilles de menthe', '1/2 citron vert', '10 glaçons']
+			},
+			link:'/about'
+		},
+		pinaColada: {
+				name: 'Piña Colada',
+				desc:'Aenean eget mattis lectus. Proin id sollicitudin nisi. Nullam vitae diam vestibulum, pulvinar nulla nec, pellentesque magna. Aenean varius ut lacus laoreet sollicitudin. Morbi et tortor rutrum, rutrum metus sit amet, ornare ligula. Quisque semper sapien a est vestibulum, eu porttitor velit rhoncus. Phasellus nibh orci, facilisis vel elit eu, feugiat pulvinar ipsum. Praesent at magna augue.',
+				ingredient:{
+					alcohol:['4cl de rhum blanc'],
+					juice : ['1 eau gazeuse', '2cl de sirop de sucre de canne'],
+					other:['6 feuilles de menthe', '1/2 citron vert', '10 glaçons']
+				},
+				link:'/about'
+			},
+		americano: {
+				name: 'Americano',
+				desc:'Aenean eget mattis lectus. Proin id sollicitudin nisi. Nullam vitae diam vestibulum, pulvinar nulla nec, pellentesque magna. Aenean varius ut lacus laoreet sollicitudin. Morbi et tortor rutrum, rutrum metus sit amet, ornare ligula. Quisque semper sapien a est vestibulum, eu porttitor velit rhoncus. Phasellus nibh orci, facilisis vel elit eu, feugiat pulvinar ipsum. Praesent at magna augue.',
+				ingredient:{
+					alcohol:['4cl de rhum blanc'],
+					juice : ['1 eau gazeuse', '2cl de sirop de sucre de canne'],
+					other:['6 feuilles de menthe', '1/2 citron vert', '10 glaçons']
+				},
+				link:'/about'
+			},
+		aperolSpritz: {
+				name: 'Aperol Spritz',
+				desc:'Aenean eget mattis lectus. Proin id sollicitudin nisi. Nullam vitae diam vestibulum, pulvinar nulla nec, pellentesque magna. Aenean varius ut lacus laoreet sollicitudin. Morbi et tortor rutrum, rutrum metus sit amet, ornare ligula. Quisque semper sapien a est vestibulum, eu porttitor velit rhoncus. Phasellus nibh orci, facilisis vel elit eu, feugiat pulvinar ipsum. Praesent at magna augue.',
+				ingredient:{
+					alcohol:['4cl de rhum blanc'],
+					juice : ['1 eau gazeuse', '2cl de sirop de sucre de canne'],
+					other:['6 feuilles de menthe', '1/2 citron vert', '10 glaçons']
+				},
+				link:'/about'
+			},
+		caipirinha: {
+				name: 'Caipirinha',
+				desc:'Aenean eget mattis lectus. Proin id sollicitudin nisi. Nullam vitae diam vestibulum, pulvinar nulla nec, pellentesque magna. Aenean varius ut lacus laoreet sollicitudin. Morbi et tortor rutrum, rutrum metus sit amet, ornare ligula. Quisque semper sapien a est vestibulum, eu porttitor velit rhoncus. Phasellus nibh orci, facilisis vel elit eu, feugiat pulvinar ipsum. Praesent at magna augue.',
+				ingredient:{
+					alcohol:['4cl de rhum blanc'],
+					juice : ['1 eau gazeuse', '2cl de sirop de sucre de canne'],
+					other:['6 feuilles de menthe', '1/2 citron vert', '10 glaçons']
+				},
+				link:'/about'
+			},
+		margarita: {
+				name: 'Margarita',
+				desc:'Aenean eget mattis lectus. Proin id sollicitudin nisi. Nullam vitae diam vestibulum, pulvinar nulla nec, pellentesque magna. Aenean varius ut lacus laoreet sollicitudin. Morbi et tortor rutrum, rutrum metus sit amet, ornare ligula. Quisque semper sapien a est vestibulum, eu porttitor velit rhoncus. Phasellus nibh orci, facilisis vel elit eu, feugiat pulvinar ipsum. Praesent at magna augue.',
+				ingredient:{
+					alcohol:['4cl de rhum blanc'],
+					juice : ['1 eau gazeuse', '2cl de sirop de sucre de canne'],
+					other:['6 feuilles de menthe', '1/2 citron vert', '10 glaçons']
+				},
+				link:'/about'
+			},
 
-		}
 	})
 
 }
