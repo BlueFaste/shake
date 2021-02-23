17577c951a9e957fae793a258a93402049e91f6f
Caroline Fassot <fassot.caroline@gmail.com>
2021-02-23 22:06:35 +0100

    retouche des titles

--- src/components/RecipesPresentation.vue
+++ src/components/RecipesPresentation.vue
@@ -3,7 +3,7 @@
 		<router-link :to="recipe.link">
 			<div class="position-relative content-box d-flex">
 				<img alt='' :src="imgLink" class="opacity-90 img-shadow">
-				<h5 class="position-absolute ml-3 mt-2 text-white">{{recipe.name}}</h5>
+				<h3 class="position-absolute ml-3 mt-2 text-white">{{recipe.name}}</h3>
 
 			</div>
 		</router-link>
@@ -23,6 +23,10 @@ export default {
 <style scoped>
 .opacity-90{
 	opacity: 0.90;
+}
+
+h3{
+	font-size: 5.7vw;
 }
 
 </style>
