2cfc82182521ba019940c6d6cfa3c0ae029d057d
Caroline Fassot <fassot.caroline@gmail.com>
2021-02-25 09:55:41 +0100

    Suppression page about

--- src/router/index.js
+++ src/router/index.js
@@ -11,11 +11,6 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
     path: '/contact',
     name: 'Contact',
     component: () => import(/* webpackChunkName: "about" */ '../views/Contact.vue')
